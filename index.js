2ea9b1a2ecfe71ad895e95ec221f9951e36f7bcc
israeldavid76 <israeldavid@gmail.com>
2020-01-17 08:15:44 -0500

    Ultima version

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-const files = require('./lib/files');
 const fs = require('fs');
-const shell = require('shelljs')
+const inquirer  = require('inquirer');
+const clone = require('git-clone');
 
 clear();
 
@@ -13,20 +13,27 @@ console.log(
   )
 );
 
-const inquirer  = require('./lib/inquire');
-
-const run = async () => {
-  const nombrePlantilla = await inquirer.askNombreTemplate();
-  fs.mkdirSync(nombrePlantilla.template); 
-  /* Funciona muy bien con shell solo que la carpeta esta con el nombre del git 
-  shell.cd(nombrePlantilla.template);
-  shell.exec('git clone https://github.com/israeldavid/ionicTemplateCA.git')
-  copiar todos los archivos del template */
-  const clone = require('git-clone');
-  clone('https://github.com/israeldavid/ionicTemplateCA.git', nombrePlantilla.template);
+export function run() {
+  inquirer.prompt({
+    name:'template',
+    type:'input',
+    message:'Ingrese el nombre del template?',
+    default:"pharmacys",
+    validate: function( value ) {
+      if (value.length) {
+        return true;
+      } else {
+        return 'Por favor ingresa el nombre de la plantilla.';
+      }
+    }
+  })
+  .then(nombrePlantilla => {
+    fs.mkdirSync(nombrePlantilla.template); 
+    clone('https://github.com/israeldavid/ionicTemplateCA.git', nombrePlantilla.template);
   console.log("Plantilla Creada");
   console.log("Ingrese al directorio:" + nombrePlantilla.template)
   console.log("y ejecute el comando Ionic Serve");
-};
+  })
+} 
 
-run();
+
